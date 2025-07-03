0a451735d2cc6e8e5d21f235da85ca7a32dfdae4
Thomas <thomas.musset@pasteur.fr>
2025-07-03 14:53:14 +0200

    initial commit v1.0.0-a.5

---
+++ src/main/java/org/bioimageanalysis/icy/maven3/GenerateDependenciesProperties.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) 2010-2025. Institut Pasteur.
+ *
+ * This file is part of Icy.
+ * Icy is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Icy is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Icy. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.bioimageanalysis.icy.maven3;
+
+import org.apache.maven.model.Dependency;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import org.jetbrains.annotations.NotNull;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+
+/**
+ * Generate a dependencies.yaml file inside the META-INF folder that lists provided (and specified) dependency.
+ *
+ * @author Thomas Musset
+ * @version 1.0.0-a.5
+ */
+@Mojo(name = "generate-dependencies-properties", defaultPhase = LifecyclePhase.GENERATE_RESOURCES)
+public class GenerateDependenciesProperties extends AbstractMojo {
+    @Parameter(defaultValue = "${project}", required = true, readonly = true)
+    MavenProject project;
+
+    @Parameter(defaultValue = "${project.build.outputDirectory}/META-INF/${project.groupId}.${project.artifactId}/dependencies.yaml", required = true, readonly = true)
+    File outputFile;
+
+    @Parameter
+    String[] excludeGroupIds;
+
+    @Parameter
+    String[] excludeArtifactIds;
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        if (!outputFile.getParentFile().exists() && !outputFile.getParentFile().mkdirs())
+            throw new MojoFailureException("Cannot create output directory");
+
+        final List<Map<String, Object>> dependencies = getDependencies();
+        final Yaml yaml = new Yaml();
+        try (final FileWriter writer = new FileWriter(outputFile)) {
+            yaml.dump(dependencies, writer);
+        }
+        catch (final IOException e) {
+            throw new MojoExecutionException(e);
+        }
+    }
+
+    private @NotNull List<Map<String, Object>> getDependencies() {
+        final List<Dependency> dependencies = project.getDependencies();
+        final List<Map<String, Object>> result = new ArrayList<>();
+        for (final Dependency dependency : dependencies) {
+            if (resolveArtifactId(dependency) && resolveGroupId(dependency) && resolveScope(dependency)) {
+                final Map<String, Object> data = new HashMap<>();
+                data.put("groupId", dependency.getGroupId());
+                data.put("artifactId", dependency.getArtifactId());
+                data.put("version", dependency.getVersion());
+
+                result.add(data);
+            }
+        }
+        return result;
+    }
+
+    private boolean resolveGroupId(final Dependency dependency) {
+        if (excludeGroupIds == null || excludeGroupIds.length == 0)
+            return true;
+
+        return !Arrays.asList(excludeGroupIds).contains(dependency.getGroupId());
+    }
+
+    private boolean resolveArtifactId(final Dependency dependency) {
+        if (excludeArtifactIds == null || excludeArtifactIds.length == 0)
+            return true;
+
+        return !Arrays.asList(excludeArtifactIds).contains(dependency.getArtifactId());
+    }
+
+    private boolean resolveScope(final Dependency dependency) {
+        return dependency.getScope().equals("provided");
+    }
+}
---
+++ src/main/java/org/bioimageanalysis/icy/maven3/GenerateExtensionProperties.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2010-2025. Institut Pasteur.
+ *
+ * This file is part of Icy.
+ * Icy is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Icy is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Icy. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.bioimageanalysis.icy.maven3;
+
+import org.apache.maven.model.Contributor;
+import org.apache.maven.model.Developer;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Thomas Musset
+ * @version 1.0.0-a.5
+ */
+@Mojo(name = "generate-extension-properties", defaultPhase = LifecyclePhase.GENERATE_RESOURCES)
+public class GenerateExtensionProperties extends AbstractMojo {
+    @Parameter(defaultValue = "${project}", required = true, readonly = true)
+    MavenProject project;
+
+    @Parameter(defaultValue = "${project.build.outputDirectory}/META-INF/extension.yaml", required = true, readonly = true)
+    File outputFile;
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        if (!outputFile.getParentFile().exists() && !outputFile.getParentFile().mkdirs())
+                throw new MojoFailureException("Unable to create output directory");
+
+        final Yaml yaml = new Yaml();
+        try (final FileWriter fw = new FileWriter(outputFile)) {
+            final Map<String, Object> data = new HashMap<>();
+            data.put("artifactId", project.getArtifactId());
+            data.put("groupId", project.getGroupId());
+            data.put("version", project.getVersion());
+            data.put("name", project.getName());
+            data.put("description", project.getDescription());
+            data.put("organization", project.getOrganization().getName());
+            data.put("organizationUrl", project.getOrganization().getUrl());
+            data.put("url", project.getUrl());
+            data.put("scm", project.getScm().getUrl());
+
+            data.put("kernelVersion", project.getProperties().getProperty("icy.kernel.version"));
+
+            final List<Map<String, Object>> developersData = new ArrayList<>();
+            for (final Developer developer : project.getDevelopers()) {
+                final Map<String, Object> developerData = new HashMap<>();
+                developerData.put("id", developer.getId());
+                developerData.put("name", developer.getName());
+                developerData.put("email", developer.getEmail());
+                developerData.put("url", developer.getUrl());
+                developerData.put("organization", developer.getOrganization());
+                developerData.put("organizationUrl", developer.getOrganizationUrl());
+                developerData.put("timezone", developer.getTimezone());
+                developerData.put("roles", developer.getRoles());
+
+                developersData.add(developerData);
+            }
+            data.put("developers", developersData);
+
+            final List<Map<String, Object>> contributorsData = new ArrayList<>();
+            for (final Contributor contributor : project.getContributors()) {
+                final Map<String, Object> contributorData = new HashMap<>();
+                contributorData.put("name", contributor.getName());
+                contributorData.put("email", contributor.getEmail());
+                contributorData.put("url", contributor.getUrl());
+                contributorData.put("organization", contributor.getOrganization());
+                contributorData.put("organizationUrl", contributor.getOrganizationUrl());
+                contributorData.put("timezone", contributor.getTimezone());
+
+                contributorsData.add(contributorData);
+            }
+            data.put("contributors", contributorsData);
+
+            data.put("downloadUrl", project.getDistributionManagement().getDownloadUrl());
+            data.put("repository", project.getDistributionManagement().getRepository().getName());
+            data.put("repositoryUrl", project.getDistributionManagement().getRepository().getUrl());
+
+            yaml.dump(data, fw);
+        }
+        catch (final IOException e) {
+            throw new MojoExecutionException(e);
+        }
+    }
+}
